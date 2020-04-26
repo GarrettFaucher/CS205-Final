e9611dce3ae3c996922c4700798b314a6d1120d1
thomassheboy <56599643+thomassheboy@users.noreply.github.com>
2020-04-26 17:37:26 -0400

    import crontab in make_plots

--- automation.js
+++ automation.js
@@ -1,6 +1,6 @@
 // Runs genplots.py on each 30th minute of the hour
 
-cron.schedule(“30 * * * *”, function () {
+cron.schedule(“30 * * * *”, function() {
   console.log(“Running Cron Job”);
   const exec = require('child_process').exec, child;
   const cliCommand = exec('python make_plots.py');
